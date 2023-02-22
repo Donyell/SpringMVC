7a31b8b1d1055c88f1900a81e16f0a8c92bbec54
Donyell Margaret <donyell@donyell.nl>
2023-02-22 16:35:15 +0100

    Add custom CourseCode validation annotation

--- src/main/java/nl/donyell/springmvc/model/Customer.java
+++ src/main/java/nl/donyell/springmvc/model/Customer.java
@@ -1,6 +1,7 @@
 package nl.donyell.springmvc.model;
 
 import jakarta.validation.constraints.*;
+import nl.donyell.springmvc.validation.CourseCode;
 
 public class Customer {
 
@@ -16,6 +17,9 @@ public class Customer {
 
     @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "Only five digits or characters.")
     private String postalCode;
+
+    @CourseCode(value = "TOPS", message = "Code must start with TOPS.")
+    private String courseCode;
 
     public String getFirstName() {
         return firstName;
@@ -47,5 +51,13 @@ public class Customer {
 
     public void setPostalCode(String postalCode) {
         this.postalCode = postalCode;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
     }
 }
---
+++ src/main/java/nl/donyell/springmvc/validation/CourseCode.java
@@ -0,0 +1,27 @@
+package nl.donyell.springmvc.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = CourseCodeConstraintValidator.class)
+@Target({ElementType.FIELD, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CourseCode {
+
+    // Define default course code
+    String value() default "LUV";
+
+    // Define default error message
+    String message() default "Must start with LUV";
+
+    // Define default groups
+    Class<?>[] groups() default {};
+
+    // Define default payloads
+    Class<? extends Payload>[] payload() default {};
+}
